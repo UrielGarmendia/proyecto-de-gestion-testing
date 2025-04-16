50f07bfd3cedd31d0c29afac60bf9492677e6669
UrielGarmendia <garmendiauriel@gmail.com>
2025-04-16 12:11:18 -0300

    cambie diseño al form

--- app-de-tareas/src/App.jsx
+++ app-de-tareas/src/App.jsx
@@ -5,7 +5,6 @@ export default function App() {
   return (
     <>
       <div>
-        <h2 class="text-3xl font-bold text-white mb-4">Mis Tareas</h2>
         <FormularioTarea />
       </div>
     </>
--- app-de-tareas/src/components/FormularioTarea.jsx
+++ app-de-tareas/src/components/FormularioTarea.jsx
@@ -8,19 +8,20 @@ export default function FormularioTarea() {
 
   return (
     <>
-      <form class="max-w-sm mx-auto text-center bg-zinc-900/60 backdrop-blur-md border border-white/10 rounded-2xl p-6 shadow-[inset_0_1px_1px_rgba(255,255,255,0.05),_0_8px_16px_rgba(0,0,0,0.3)] hover:-translate-y-1 transition">
+      <form class="max-w-sm mx-auto text-center ">
+        <h2 class="text-3xl font-bold text-white mb-4">Mis Tareas</h2>
         <div class="task-title">
           <label
-            for="email"
+            for="text"
             class="block mb-2 text-sm font-medium text-white text-left"
           >
             Titulo de la tarea
           </label>
           <input
-            type="email"
-            id="email"
+            type="text"
+            id="text"
             autoComplete="off"
-            class="bg-zinc-800 text-white placeholder-gray-400 rounded-lg p-3 shadow-inner focus:outline-none focus:ring-2 focus:gray-400 transition text-sm block w-full border-gray-600"
+            class="rounded-[20px] bg-[#7f7a7a] [box-shadow:-20px_20px_60px_#6c6868,_20px_-20px_60px_#928c8c] border-[none] p-[15px] pr-[30px] m-[6px] text-white placeholder-gray-400 shadow-inner focus:outline-none focus:ring-2 focus:gray-400 transition text-sm block w-full"
             placeholder="Ej: Llamar cliente"
           />
         </div>
@@ -36,7 +37,7 @@ export default function FormularioTarea() {
             options={{
               dateFormat: "d/m/Y",
             }}
-            class="bg-zinc-800 text-white placeholder-gray-400 rounded-lg p-3 shadow-inner focus:outline-none focus:ring-2 focus:gray-400 transition border-gray-600 text-sm focus:border-blue-500 block w-full"
+            class="rounded-[20px] bg-[#7f7a7a] [box-shadow:-20px_20px_60px_#6c6868,_20px_-20px_60px_#928c8c] border-[none] p-[15px] pr-[30px] m-[6px] text-white placeholder-gray-400 shadow-inner focus:outline-none focus:ring-2 focus:gray-400 transition text-sm focus:border-slate-500 block w-full"
             value={date}
             onChange={([selectedDate]) => setDate(selectedDate)}
           />
@@ -51,7 +52,7 @@ export default function FormularioTarea() {
           </label>
           <select
             id="priority"
-            class="bg-zinc-800 text-white placeholder-gray-400 rounded-lg p-3 shadow-inner focus:outline-none focus:ring-2 focus:gray-400 transition text-sm block w-full border-gray-600 focus:border-blue-500"
+            class="rounded-[20px] bg-[#7f7a7a] [box-shadow:-20px_20px_60px_#6c6868,_20px_-20px_60px_#928c8c] border-[none] p-[15px] pr-[30px] m-[6px] text-white placeholder-gray-400  shadow-inner focus:outline-none focus:ring-2 focus:gray-400 transition text-sm block w-full  focus:border-blue-500"
             value={opcion}
             onChange={(e) => setOpcion(e.target.value)}
           >
@@ -72,14 +73,14 @@ export default function FormularioTarea() {
           <textarea
             id="description"
             rows="5"
-            class="bg-zinc-800 text-white placeholder-gray-400 rounded-lg p-3 shadow-inner focus:outline-none focus:ring-2 focus:ring-gray-400 transition resize-none block w-full text-sm border-gray-600 focus:border-gray-400"
+            class="rounded-[20px] bg-[#7f7a7a] [box-shadow:-20px_20px_60px_#6c6868,_20px_-20px_60px_#928c8c] border-[none] p-[15px] pr-[30px] m-[6px] text-white placeholder-gray-400 shadow-inner focus:outline-none focus:ring-2 focus:ring-gray-400 transition resize-none block w-full text-sm focus:border-gray-400"
             placeholder="Ej: Llamar al cliente para confirmar la reunión del jueves a las 10:00."
           ></textarea>
         </div>
         <div class="submit-task mt-2">
           <button
             type="submit"
-            class="text-white focus:outline-none focus:ring-4 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mt-2 bg-zinc-800 hover:bg-gray-700 focus:ring-gray-400 border-gray-700"
+            class="rounded-[20px] bg-[#7f7a7a] [box-shadow:-20px_20px_60px_#6c6868,_20px_-20px_60px_#928c8c] border-[none] p-[15px] pr-[30px] m-[6px] text-white focus:outline-none focus:ring-4 font-medium  text-sm px-5 py-2.5 me-2 mt-2 hover:bg-[#5f5f5f] focus:ring-gray-400"
           >
             Agregar tarea
           </button>
